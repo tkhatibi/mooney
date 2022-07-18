4dee52de74950ee877e75147d48558b4f598971f
Touraj Khatibi <2rajpx@gmail.com>
2022-07-19 01:00:20 +0200

    fix(docker): permissions on linux

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 description() {
-  echo "If it's your first usage, it initializes the project; else resets everything";
+    echo "If it's your first usage, it initializes the project; else resets everything";
 }
 
 help() {
-  echo "Available Options:"
+    echo "Available Options:"
 
-  echo "  -r"
-  echo "  Removes everything before start"
-  echo
+    echo "  -r"
+    echo "  Removes everything before start"
+    echo
 }
 
 run() {
-  if [[ " ${@:1} " =~ " -r " ]]; then
-    $APP dc down -v --remove-orphans
-    $APP dc build --no-cache
-  fi
+    if [[ " ${@:1} " =~ " -r " ]]; then
+        $APP dc down -v --remove-orphans
+        $APP dc build --no-cache
+    fi
 
-  $APP dcu
+    # https://github.com/dunglas/symfony-docker/blob/main/docs/troubleshooting.md#editing-permissions-on-linux
+    $APP dc run --rm php chown -R $(id -u):$(id -g) .
+
+    $APP dcu
 }
