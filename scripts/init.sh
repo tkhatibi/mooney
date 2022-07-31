4d17c721f04cd9ac778d98731606c65bad283a98
Touraj <2rajpx@gmail.com>
2022-08-01 02:19:26 +0400

    fix: chromebrave ssl on init

--- scripts/init.sh
+++ scripts/init.sh
@@ -24,4 +24,11 @@ run() {
     $APP dc run --rm php chown -R $(id -u):$(id -g) .
 
     $APP dcu
+
+    # https://github.com/dunglas/symfony-docker/blob/main/docs/troubleshooting.md#fix-chromebrave-ssl
+    if [ "$OS" = 'Linux' ]; then
+        docker cp $($APP dc ps -q caddy):/data/caddy/pki/authorities/local/root.crt /usr/local/share/ca-certificates/root.crt && sudo update-ca-certificates
+    elif [ "$OS" = 'Darwin' ]; then
+        docker cp $($APP dc ps -q caddy):/data/caddy/pki/authorities/local/root.crt /tmp/root.crt && sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /tmp/root.crt
+    fi
 }
